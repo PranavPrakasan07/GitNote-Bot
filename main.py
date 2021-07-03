7fc49a40c14a67ca76866598d32b3dd2892dd820
Pranav Prakasan <59109508+PranavPrakasan07@users.noreply.github.com>
2021-07-03 09:38:37 +0000

    help embed added

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ import discord
 import requests
 import json
 from keep_alive import keep_alive
+from replit import db
 
 my_secret = os.environ['TOKEN']
 
 client = discord.Client()
+
+
 
 @client.event
 async def on_ready():
@@ -16,7 +19,6 @@ async def on_ready():
 async def on_message(message):
   if message.author == client.user:
     return
-
 
   if((str(message.author) == 'GitHub#0000')):
 
@@ -35,10 +37,17 @@ async def on_message(message):
     await message.channel.send('Hello!' +  f"{message.author.mention}")
 
   if message.content.startswith('&help'):      
+    await message.delete()
 
     help_message = "&hello - Hello\n&help - Help\n"
 
-    await message.channel.send("\n**Commands**\n\n>>> {}".format(help_message))
+    embed=discord.Embed(title="\n**Commands**\n\n",  description=help_message, color=0xFF5733)
+    embed.set_author(name = "ISA-VIT", icon_url = "https://firebasestorage.googleapis.com/v0/b/isa-vit.appspot.com/o/icon.png?alt=media&token=c8fbffbb-ab51-4b49-a83b-8fb40626d8f8")
+    
+    await message.channel.send(embed=embed)
+
+    # await message.channel.send("\n**Commands**\n\n>>> {}".format(help_message))
+
 
 keep_alive()
 client.run(my_secret)
